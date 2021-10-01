d2ec0aa90a5d3bfa8c7ca9c30be19c552f8ccdf8
Cooper Santillan <maggieandme117@gmail.com>
2021-10-01 00:38:49 -0700

    Error handling

--- Settings/CALLER.lua
+++ Settings/CALLER.lua
@@ -53,16 +53,30 @@ function maker.caller()
 		return false;
 	end
 
+	local uflag = true	-- user flag, true if no user was found in match
+	local tflag = true	-- task flag, true if no task was found in match
+
 	-- cyle through users and determine which one is to be used
 	for k in pairs(localUser) do
 		if (k:match(UserVar) then
+			uflag = false
 			for j in pairs(maker.task) do
 				if(j:match(taskVar) then
 					localUser[k].self = k
 					maker.task[j](localUser[k])
+					tflag = false
 				end
 			end
 		end
+	end
+
+	-- prompt the user if there was an error
+	if(uflag and tflag) then
+		gma.gui.msgbox("ERROR in CALLER Plugin", "The user and program you tried using were not found.\n You set " ..UserVar .." as a user and " ..taskVar .." as the program to use.")
+	elseif(uflag) then
+		gma.gui.msgbox("ERROR in CALLER Plugin", "The user you tried using were not found.\n You tried using " ..UserVar .." as a user but it does not exist.")
+	elseif(tflag) then
+		gma.gui.msgbox("ERROR in CALLER Plugin", "The program you tried using were not found.\n You tried using the " ..taskVar .." program but it does not exist.")
 	end
 
 end
